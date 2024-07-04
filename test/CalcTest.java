e5cb18dbacdb0f3e4358ce580b07249cc4eb5f72
agabriel01314 <xailuz4@gmail.com>
2024-07-04 12:37:11 -0600

    se subio el calc

---
+++ test/CalcTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import src.calc;
+
+public class CalcTest {
+
+    private calc calculadora;
+
+    @BeforeEach
+    public void setUp() {
+        calculadora = new calc();
+    }
+
+    @Test
+    public void testSumar() {
+        assertEquals(5, calculadora.suma(2, 3));
+        assertEquals(0, calculadora.suma(-2, 2));
+        assertEquals(-5, calculadora.suma(-2, -3));
+    }
+
+    private void assertEquals(int i, int suma) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'assertEquals'");
+    }
+
+    @Test
+    public void testRestar() {
+        assertEquals(1, calculadora.resta(3, 2));
+        assertEquals(-4, calculadora.resta(-2, 2));
+        assertEquals(1, calculadora.resta(-2, -3));
+    }
+
+    @Test
+    public void testMultiplicar() {
+        assertEquals(6, calculadora.multiplicacion(2, 3));
+        assertEquals(-4, calculadora.multiplicacion(-2, 2));
+        assertEquals(6, calculadora.multiplicacion(-2, -3));
+    }
+}
